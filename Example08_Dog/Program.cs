fb0e4cc8db239da4632ff54d3cbf2335c10c8e21
Anatoliy K <778966@mail.ru>
2022-09-14 17:32:48 +0500

    added Example08 about dog project

---
+++ Example08_Dog/Program.cs
@@ -0,0 +1,25 @@
+﻿int distance = 1000;
+int firstFriendS = 20;
+int secondFriendS = 10;
+int dogS = 30;
+int friend = 2;
+int count = 0;
+int time = 1;
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance/(firstFriendS+dogS );
+        friend = 2;
+    }
+    else
+    {
+        time = distance/(secondFriendS + dogS );
+        friend = 1;
+        distance = distance - (firstFriendS + secondFriendS)*time;
+    }
+    count++;
+}
+
+Console.Write("count = ");
+Console.WriteLine(count);
