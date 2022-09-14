4bb275dee80fe33b2d15c3cc84befaf4239455b8
Anatoliy K <778966@mail.ru>
2022-09-14 22:18:59 +0500

    added Example08 dog project

--- Example08_Dog/Program.cs
+++ Example08_Dog/Program.cs
@@ -1,23 +1,23 @@
-﻿int distance = 1000;
+﻿float distance = 30000;
 int firstFriendS = 20;
 int secondFriendS = 10;
 int dogS = 30;
 int friend = 2;
 int count = 0;
-int time = 1;
+float time = 1;
 while (distance > 10)
 {
     if (friend == 1)
     {
-        time = distance/(firstFriendS+dogS );
+        time = distance/(firstFriendS + dogS );
         friend = 2;
     }
     else
     {
         time = distance/(secondFriendS + dogS );
         friend = 1;
-        distance = distance - (firstFriendS + secondFriendS)*time;
     }
+    distance = distance - (firstFriendS + secondFriendS)*time;
     count++;
 }
 
