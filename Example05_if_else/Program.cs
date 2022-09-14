5d9dafc5bbc5a7d4fb3eaab251bfe023d52f02a3
Anatoliy K <778966@mail.ru>
2022-09-14 13:36:03 +0500

    added Example05 if alse project

---
+++ Example05_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Add user name: ");
+String username = Console.ReadLine();
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Very Beutiful MASHA ");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
